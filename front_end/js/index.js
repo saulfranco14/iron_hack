796ea7905cd14e12b608935553ddd2dc04765ad5
saulfranco14 <saul.franco1420@gmail.com>
2023-03-05 23:20:14 -0600

    Delete code in front-end

--- front_end/js/index.js
+++ front_end/js/index.js
@@ -1,11 +1,1 @@
 const url = 'https://api.npoint.io/d6f34835d78c362d3636';
-
-fetch(url)
-  .then(response => response.json())
-  .then(data => {
-    const users = data.users;
-    const usersList = users.map(user => `<li>${user.name}</li>`).join('');
-    const usersContainer = document.getElementById('users');
-    usersContainer.innerHTML = `<ul>${usersList}</ul>`;
-  })
-  .catch(error => console.log(error));
